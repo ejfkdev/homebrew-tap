900e3236c41d0cec818edf03945cd9580c67380d
ejfkdev <152914458+ejfkdev@users.noreply.github.com>
2026-08-08 17:10:59 +0800

    chore: bump oss formula to v0.1.2

--- Formula/oss.rb
+++ Formula/oss.rb
@@ -1,27 +1,27 @@
 class Oss < Formula
   desc "S3-compatible cross-cloud object storage CLI (AWS / Aliyun / Tencent / Huawei / Qiniu / GCS / R2 / MinIO ...)"
   homepage "https://github.com/ejfkdev/oss"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.1.1/oss-v0.1.1-darwin-arm64.tar.gz"
-      sha256 "9b2799bf182ea333e187ebf3d897798ca0be2cc2c6e9d08172764b4786b410cc"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.1.2/oss-v0.1.2-darwin-arm64.tar.gz"
+      sha256 "78704569905bf6cfbc44735cad76320eed3bd728287bad90dd50032cea7b306e"
     end
     on_intel do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.1.1/oss-v0.1.1-darwin-amd64.tar.gz"
-      sha256 "f0c88b1a86b5548b32f87f2fb14c9991946ca8b8fa4eac2ec7cf01fd30bb9b88"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.1.2/oss-v0.1.2-darwin-amd64.tar.gz"
+      sha256 "f0af03b59c6d2c1cd8cf2a3cdf0412cf4228040fc841f1c1c01b13d213218df5"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.1.1/oss-v0.1.1-linux-arm64.tar.gz"
-      sha256 "ab346b8df4a235cebfbb0219d3ed945d57f9ad8836d6b7a31d97678c436c3840"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.1.2/oss-v0.1.2-linux-arm64.tar.gz"
+      sha256 "40e0edef34da08caa7014390e1b10a346f239a4d174ca7f2f8cea82c4010173d"
     end
     on_intel do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.1.1/oss-v0.1.1-linux-amd64.tar.gz"
-      sha256 "5e6c262bdf6fda616ae0cce41e01b489f9e645872814d7191d321a3fc8a1fdc7"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.1.2/oss-v0.1.2-linux-amd64.tar.gz"
+      sha256 "d31011f539fa491664ce4467d6508933d037c6d7b8e4e12091230071711a3422"
     end
   end
 
