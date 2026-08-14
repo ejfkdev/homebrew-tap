ee79618ba562876d4dbd728bb334a1680975cc39
ejfkdev <152914458+ejfkdev@users.noreply.github.com>
2026-08-14 04:47:54 +0000

    auto-update oss

--- Formula/oss.rb
+++ Formula/oss.rb
@@ -1,27 +1,27 @@
 class Oss < Formula
   desc "S3-compatible cross-cloud object storage CLI (AWS / Aliyun / Tencent / Huawei / Qiniu / GCS / R2 / MinIO ...)"
   homepage "https://github.com/ejfkdev/oss"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
     on_arm do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.1.2/oss-v0.1.2-darwin-arm64.tar.gz"
-      sha256 "78704569905bf6cfbc44735cad76320eed3bd728287bad90dd50032cea7b306e"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.1.3/oss-0.1.3-darwin-arm64.tar.gz"
+      sha256 "MISSING"
     end
     on_intel do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.1.2/oss-v0.1.2-darwin-amd64.tar.gz"
-      sha256 "f0af03b59c6d2c1cd8cf2a3cdf0412cf4228040fc841f1c1c01b13d213218df5"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.1.3/oss-0.1.3-darwin-amd64.tar.gz"
+      sha256 "MISSING"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.1.2/oss-v0.1.2-linux-arm64.tar.gz"
-      sha256 "40e0edef34da08caa7014390e1b10a346f239a4d174ca7f2f8cea82c4010173d"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.1.3/oss-0.1.3-linux-arm64.tar.gz"
+      sha256 "MISSING"
     end
     on_intel do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.1.2/oss-v0.1.2-linux-amd64.tar.gz"
-      sha256 "d31011f539fa491664ce4467d6508933d037c6d7b8e4e12091230071711a3422"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.1.3/oss-0.1.3-linux-amd64.tar.gz"
+      sha256 "MISSING"
     end
   end
 
