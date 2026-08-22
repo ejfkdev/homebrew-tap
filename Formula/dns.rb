1868defdbacfa585ccdb4eefec7f4c0e3b44852f
ejfkdev <152914458+ejfkdev@users.noreply.github.com>
2026-08-22 12:43:58 +0000

    auto-update dns

--- Formula/dns.rb
+++ Formula/dns.rb
@@ -1,27 +1,27 @@
 class Dns < Formula
   desc "Multi-server DNS query CLI (concurrent, all record types, DoT/DoH/DoQ/HTTPDNS)"
   homepage "https://github.com/ejfkdev/dns"
-  version "0.1.4"
+  version "0.3.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/ejfkdev/dns/releases/download/v0.1.4/dns-0.1.4-aarch64-apple-darwin"
-      sha256 "48c3de20c58ce743e68d559faa53c784ab105e9e82c355974bbf20b53c6ef1f0"
+      url "https://github.com/ejfkdev/dns/releases/download/v0.3.0/dns-0.3.0-aarch64-apple-darwin"
+      sha256 "443e5ee8c17a4cfdf6580b7a0a74aca85f33b6d0f226c5a4a4193e374e67bdfb"
     end
     on_intel do
-      url "https://github.com/ejfkdev/dns/releases/download/v0.1.4/dns-0.1.4-x86_64-apple-darwin"
-      sha256 "753fc33d0cfcb2a7726d33ca24ba20f788c536248014989fc203b3fbf2fed07d"
+      url "https://github.com/ejfkdev/dns/releases/download/v0.3.0/dns-0.3.0-x86_64-apple-darwin"
+      sha256 "e6e7c5ad86b8b6878fca33960024365f0c7c075e419f49104b4d4bbf371887e7"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ejfkdev/dns/releases/download/v0.1.4/dns-0.1.4-aarch64-unknown-linux-gnu"
-      sha256 "da4ffb2560bd002a44078c13abeba106af54035d1701b429305120d54c4fb36b"
+      url "https://github.com/ejfkdev/dns/releases/download/v0.3.0/dns-0.3.0-aarch64-unknown-linux-gnu"
+      sha256 "28d15a197caf9fee59bfc2a54b79c1d958643b2f30a661e38f3991bad6ebfc65"
     end
     on_intel do
-      url "https://github.com/ejfkdev/dns/releases/download/v0.1.4/dns-0.1.4-x86_64-unknown-linux-gnu"
-      sha256 "76d699bf16cddfb3ff76ec1dd0fd4a8dff3facb536dfe98464b504ed6f9e3a79"
+      url "https://github.com/ejfkdev/dns/releases/download/v0.3.0/dns-0.3.0-x86_64-unknown-linux-gnu"
+      sha256 "b719a71ecbdcb2944f5f1cb3ea0aef22f9ead0f5cec1b16701e3f77a403ae090"
     end
   end
 
