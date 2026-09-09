5ca72a24111784e3e4be179e70674c2998a225e8
ejfkdev <152914458+ejfkdev@users.noreply.github.com>
2026-09-09 07:56:49 +0000

    auto-update oss

--- Formula/oss.rb
+++ Formula/oss.rb
@@ -1,27 +1,27 @@
 class Oss < Formula
   desc "S3-compatible cross-cloud object storage CLI (AWS / Aliyun / Tencent / Huawei / Qiniu / GCS / R2 / MinIO ...)"
   homepage "https://github.com/ejfkdev/oss"
-  version "0.2.4"
+  version "0.2.5"
 
   on_macos do
     on_arm do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.2.4/oss-v0.2.4-darwin-arm64.tar.gz"
-      sha256 "bdbdeb0df2581f215193fdde240a3cb25c4ebd5c867c96f179cd7ba6b026b6a6"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.2.5/oss-v0.2.5-darwin-arm64.tar.gz"
+      sha256 "b442378053145d3e3149bfcb440b27c30246dfc0920024a22b9a851243d5798e"
     end
     on_intel do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.2.4/oss-v0.2.4-darwin-amd64.tar.gz"
-      sha256 "56868a035f9928c14c4598aea9d4067b184617fa8cfb29af8cd988412a6bee79"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.2.5/oss-v0.2.5-darwin-amd64.tar.gz"
+      sha256 "e11faa3609f277fdffe1b046df4cfaa55360baddd05b1ff543161b7648aee4ca"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.2.4/oss-v0.2.4-linux-arm64.tar.gz"
-      sha256 "0c3baa26a3993cf80bcb6a043acf2cdf7ee44cf015d88b8b2e1bd4e8896f5a0a"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.2.5/oss-v0.2.5-linux-arm64.tar.gz"
+      sha256 "b4c1bec6395a853af7c50888fc618b0a1de1e9a925e8b7f91a54f8a3b1840670"
     end
     on_intel do
-      url "https://github.com/ejfkdev/oss/releases/download/v0.2.4/oss-v0.2.4-linux-amd64.tar.gz"
-      sha256 "958aae1cf1e02f1bb0ca5703df3f7ef0b80af672023e3f9d4e270cb0dc9e1170"
+      url "https://github.com/ejfkdev/oss/releases/download/v0.2.5/oss-v0.2.5-linux-amd64.tar.gz"
+      sha256 "e105c4c55d87c9fdf338f33008f96616919398efe26691fd37b92a5debbcdc96"
     end
   end
 
